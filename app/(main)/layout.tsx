9348773860ea181fa8ab5ca170f316ea8065ab3f
eduardo-nicola <eduardonicolagit@gmail.com>
2026-04-01 20:47:03 -0300

    RESET PROJECT

--- app/(main)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ProtectedApp } from "@/components/protected-app";
-
-export default function MainGroupLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <ProtectedApp>{children}</ProtectedApp>;
-}
